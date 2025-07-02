2b6625857e71441c0fced6bbfa8cba3521234400
vyshnavi kamireddy <vyshnavikamireddy@gmail.com>
2025-07-02 11:33:16 +0530

    Final v0

--- src/Chef Claude/Ai.jsx
+++ src/Chef Claude/Ai.jsx
@@ -52,7 +52,7 @@ export default async function getRecipeFromMistral(ingredientsArr) {
     const ingredientsString = ingredientsArr.join(", ")
     try {
         const response = await hf.chatCompletion({
-            model: "mistralai/Mixtral-8x7B-Instruct-v0.1",
+            model: "HuggingFaceH4/zephyr-7b-beta",
             messages: [
                 { role: "system", content: SYSTEM_PROMPT },
                 { role: "user", content: `I have ${ingredientsString}. Please give me a recipe you'd recommend I make!` },
